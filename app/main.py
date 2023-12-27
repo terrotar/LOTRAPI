304827239e85d1cd89b346c42bf3ee41616c2f96
Pedro Mesquita Guedes <958864@lojasrenner.com.br>
2023-12-26 23:20:00 -0300

    Created branch races and start development

--- app/main.py
+++ app/main.py
@@ -1,7 +1,35 @@
 
 # Libraries
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 
 # Application
 app = FastAPI()
+
+
+
+load_dotenv()
+
+
+# Router Race
+# router = APIRouter(prefix='/races',
+#                    tags=['Race'])
+
+
+# List all races
+@app.get("/all")
+async def read_all_races(races: list):
+
+    all_races = 'url'
+
+    return all_races
+
+    # if app_poke:
+    #     return app_poke
+
+    # raise HTTPException(
+    #     status_code=400,
+    #     detail="Database is empty"
+    # )
+
