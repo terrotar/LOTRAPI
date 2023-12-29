71f2e98c864da50f91bc7bb33b7da8bd41390b6f
Pedro Mesquita Guedes <958864@lojasrenner.com.br>
2023-12-28 21:59:59 -0300

    Collected all races from The One API

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 
 # Libraries
-from fastapi import FastAPI
-from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv, dotenv_values
+import requests
+import json
 
 
 # Application
 app = FastAPI()
-
-
-
-load_dotenv()
 
 
 # Router Race
@@ -19,17 +17,26 @@ load_dotenv()
 
 # List all races
 @app.get("/all")
-async def read_all_races(races: list):
+async def read_all_races():
 
-    all_races = 'url'
+    characters_endpoint = 'https://the-one-api.dev/v2/character?wikiUrl'
 
-    return all_races
+    api_token = dotenv_values(".env")
 
-    # if app_poke:
-    #     return app_poke
+    characters_request = requests.get(characters_endpoint, headers={'Authorization': f'Bearer {api_token['THE_ONE_API_TOKEN']}'})
 
-    # raise HTTPException(
-    #     status_code=400,
-    #     detail="Database is empty"
-    # )
+    characters = json.loads(characters_request.content)['docs']
 
+    all_races = []
+    for i in characters:
+        race = i['race']
+        if race not in all_races and race != '':
+            all_races.append(race)
+
+    if characters:
+        return all_races
+
+    raise HTTPException(
+        status_code=503,
+        detail="The One API connection failed"
+    )
